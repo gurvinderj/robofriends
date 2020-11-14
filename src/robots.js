822811a12d17c3900d0b6df0eba35750a190f07b
gurvinderj <gurvinderjohal96@outlook.com>
2020-11-14 13:42:14 +0000

    final commit

--- src/robots.js
+++ src/robots.js
@@ -1,9 +1,9 @@
 export const robots = [
   {
     id: 1,
-    name: "Leanne Graham",
-    username: "Bret",
-    email: "Sincere@april.biz",
+    name: "Barms",
+    username: "Barms",
+    email: "Stinky@qtpie.com",
   },
   {
     id: 2,
